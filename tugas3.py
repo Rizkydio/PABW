f6cf38b162f2ca0ff480b200ddfcb95863c124f1
rizkydio <rizkysandi39@google.com>
2023-03-10 10:57:45 +0700

    v1.1

---
+++ tugas3.py
@@ -0,0 +1,11 @@
+#1
+#membuat dictionary orang
+orang = [
+    {'nama': 'Arsene Lupin', 'nik': 1234, 'jenis_kelamin': 'male', 'tanggal_lahir': '1902-03-23'},
+    {'nama': 'Sherlock Holmes', 'nik': 4321, 'jenis_kelamin': 'male', 'tanggal_lahir': '1876-08-16'},
+    {'nama': 'Irene Adler', 'nik': 6789, 'jenis_kelamin': 'female', 'tanggal_lahir': '1884-10-07'}
+]
+
+#2
+# menerima input dari user
+nama_dicari = input("Masukkan nama yang ingin dicari: ")
